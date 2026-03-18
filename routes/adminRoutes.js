0ee7c9fc2114b1cd9a3ce0750c4a85f5e39f597f
Kevin Malou <maloukevin6@gmail.com>
2026-03-18 09:52:30 +0200

    i added adminroute

---
+++ routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { getAllTickets, updateTicketStatus, adminReply, getStats } = require('../controllers/adminController');
+const { protect } = require('../middleware/authMiddleware');
+const { adminOnly } = require('../middleware/adminMiddleware');
+
+
+router.get('/tickets', protect, adminOnly, getAllTickets);
+router.put('/tickets/:id/status', protect, adminOnly, updateTicketStatus);
+router.post('/tickets/:id/reply', protect, adminOnly, adminReply);
+router.get('/stats', protect, adminOnly, getStats);
+
+module.exports = router;
