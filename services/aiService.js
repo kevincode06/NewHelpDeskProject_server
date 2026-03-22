3eb9c3ce16e95488f62862953a8b1780c3a2dc52
Kevin Malou <maloukevin6@gmail.com>
2026-03-22 08:17:47 +0200

    i fixed

--- services/aiService.js
+++ services/aiService.js
@@ -1,7 +1,7 @@
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-    apiKey: process.env.AI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY
 });
 
 const getAIResponse = async (userMessage, ticketContext = []) => {
